cd45c3c5199ec6c74ea6686f2852441aabfff356
thomas-fan <youxiboy007@vip.qq.com>
2019-04-19 17:31:06 +0800

    完成tab切换栏

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PriceList from './components/PriceList'
+import ViewTab from './components/ViewTab'
 
 const items = [
     {
@@ -33,27 +34,11 @@ const items = [
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-                <PriceList items={items} onModifyItem={(item) => {
-                    console.log(item)
-                }} onDeleteItem={(item) => {
-                    console.log(item)
-                }}>
-                </PriceList>
+            <div>
+                <ViewTab activeTab="list" onTabChange={(view) => {
+                    console.log(view)}}>
+
+                </ViewTab>
             </div>
         );
     }
